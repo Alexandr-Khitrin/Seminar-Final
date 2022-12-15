27f08d149814dfda07c422522ba882da79ff5c07
Alexandr <hitrin79@gmail.com>
2022-12-15 22:00:04 +0800

    Zadacha 68

---
+++ Zadacha 68/Zadacha 68/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите первое число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Akkerman(m, n));
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman (m, n - 1)));
+    }
+}
