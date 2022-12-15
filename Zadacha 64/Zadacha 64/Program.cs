da02fb9b969a8186d0cdba922699bff6867731a5
Alexandr <hitrin79@gmail.com>
2022-12-15 18:57:20 +0800

    Zadacha 64

---
+++ Zadacha 64/Zadacha 64/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+NaturalNumber(number);
+
+void NaturalNumber(int number)
+{
+    if (number < 0)
+    {
+        Console.WriteLine("Вы ввели не натуральное число");
+        return;
+    }    
+    if (number == 0) return;
+    Console.Write(number + " ");
+    NaturalNumber(number - 1);
+}
