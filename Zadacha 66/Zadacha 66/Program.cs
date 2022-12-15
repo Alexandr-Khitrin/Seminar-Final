ce86eb4b248af903470e20347464610c476e2e8c
Alexandr <hitrin79@gmail.com>
2022-12-15 21:38:04 +0800

    Zadacha 66

---
+++ Zadacha 66/Zadacha 66/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int n = int.Parse(Console.ReadLine()!);
+NaturalSum(m, n, 0);
+void NaturalSum (int m, int n, int sum)
+{
+    if (m > n)
+    {
+        Console.WriteLine($"Сумма элементов: {sum}");
+        return;
+    }
+    sum = sum + m++;
+    NaturalSum(m, n, sum);
+}
